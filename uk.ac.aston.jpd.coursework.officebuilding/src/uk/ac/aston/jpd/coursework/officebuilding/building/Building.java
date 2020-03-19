3a5e20b4b2d8e9fd633d548baba9049726243f29
jaappiah <61872777+jaappiah@users.noreply.github.com>
2020-03-19 20:30:44 +0000

    Update Building.java

--- uk.ac.aston.jpd.coursework.officebuilding/src/uk/ac/aston/jpd/coursework/officebuilding/building/Building.java
+++ uk.ac.aston.jpd.coursework.officebuilding/src/uk/ac/aston/jpd/coursework/officebuilding/building/Building.java
@@ -1,9 +1,28 @@
 package uk.ac.aston.jpd.coursework.officebuilding.building;
 
-import uk.ac.aston.jpd.coursework.officebuilding.building.elevator.Elevator;
-
 public class Building {
-	public Building(Elevator e) {
+	//declaring fields
+	private List <Floor> floors = new ArrayList <Floor>();;
+	private Elevator elevator;
+	
+	//constuctor
+	public Building (int floors){
+	this.floors = floors; 
 	}
 	
+	public void tick(){
+	elevator.tick();
+		for (Floor f : floors){
+		f.tick();
+		}
+	}
+	private int generateFloors(){
+		for(int i = 0; i <8; i++){
+			Floor f = new Floor(i);
+			floor.add(f);
+		}
+	}
+	public int getNumberOfFloor(){
+	return floors.size();
+	}
 }
