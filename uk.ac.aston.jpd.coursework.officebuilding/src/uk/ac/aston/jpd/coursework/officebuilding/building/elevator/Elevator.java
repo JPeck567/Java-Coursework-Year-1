53e884c0f6c486c02383f6903ca9439fed29280f
Jorge <61948082+JPeck567@users.noreply.github.com>
2020-03-19 02:33:42 +0000

    Mostly coded Elevator and added Person superclass

--- uk.ac.aston.jpd.coursework.officebuilding/src/uk/ac/aston/jpd/coursework/officebuilding/building/elevator/Elevator.java
+++ uk.ac.aston.jpd.coursework.officebuilding/src/uk/ac/aston/jpd/coursework/officebuilding/building/elevator/Elevator.java
@@ -1,5 +1,72 @@
 package uk.ac.aston.jpd.coursework.officebuilding.building.elevator;
 
+import uk.ac.aston.jpd.coursework.officebuilding.building.Building;
+import uk.ac.aston.jpd.coursework.officebuilding.building.floor.Floor;
+import uk.ac.aston.jpd.coursework.officebuilding.simulator.Simulator;
+import uk.ac.aston.jpd.coursework.officebuilding.person.entities.Person;
+
 public class Elevator {
+	private final int MAXCAPACITY;
+	private int currentCapacity;
+	private String state;
+	private boolean isMovement;
+	private boolean isDoorOpen;
+	private ElevatorQueue queue;
+	private int destination;
+	private int currentFloor;
+	
+	public Elevator(ElevatorQueue queue, int maxCapacity) {
+		MAXCAPACITY = maxCapacity;
+		currentCapacity = 0;
+		state = "idle";
+		isMovement = false; // true = moving, false = still
+		isDoorOpen = false; // true = open, false = closed
+		this.queue = queue;
+		destination = 0;
+		currentFloor = 0;
+	}
+	
+	public void tick(Simulator sim, Floor currentFloorObj) { // implement with switch case?
+		if(currentFloor != destination) {
+			moveFloor(currentFloor - destination);
+		} else if(!isDoorOpen) { // implies the elevator is at its destination + door is not open
+			toggleDoor();
+		} else if 
+		// TODO: from here!
+	}
+	
+	private void moveFloor (int direction) {
+		if(direction > 0) {
+			currentFloor -= 1;
+		} else {
+			currentFloor += 1;
+		}
+	}
+	
+	private void offloadPeople(Simulator sim) {
+		sim.setOffloadPeople(queue.getOffloadPeople());
+	}
+	
+	private void onloadPeople(Simulator sim, Building bld, int capacityAllowance) {
+		Floor fl = bld.getFloor(currentFloor);
+		Person[] people = fl.removeFromQueue(capacityAllowance);
+		sim.setOnloadPeople(people);
+	}
+	
+	private void toggleDoor() {
+		isDoorOpen = !isDoorOpen;
+	}
+	
+	public int getCurrentFloor() {
+		return currentFloor;
+	}
+	
+	public boolean isMoving() {
+		return isMovement;
+	}
+
+	public boolean isDoorOpen() {
+		return isDoorOpen;
+	}
 	
 }
--- uk.ac.aston.jpd.coursework.officebuilding/src/uk/ac/aston/jpd/coursework/officebuilding/building/elevator/ElevatorQueue.java
+++ uk.ac.aston.jpd.coursework.officebuilding/src/uk/ac/aston/jpd/coursework/officebuilding/building/elevator/ElevatorQueue.java
@@ -1,5 +1,14 @@
 package uk.ac.aston.jpd.coursework.officebuilding.building.elevator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ElevatorQueue {
+	private List<Integer> goingUp;
+	private List<Integer> goingDown;
 	
+	public ElevatorQueue() {
+		goingUp = new ArrayList<Integer>();
+		goingDown = new ArrayList<Integer>();
+	}
 }
