044c18d8fa5fb8abc1be13e7657b40fab7b8f30c
Jorge <61948082+JPeck567@users.noreply.github.com>
2020-04-18 20:51:29 +0100

    Update Elevator.java

--- uk.ac.aston.jpd.coursework.officebuilding/src/uk/ac/aston/jpd/coursework/officebuilding/building/elevator/Elevator.java
+++ uk.ac.aston.jpd.coursework.officebuilding/src/uk/ac/aston/jpd/coursework/officebuilding/building/elevator/Elevator.java
@@ -1,6 +1,7 @@
 package uk.ac.aston.jpd.coursework.officebuilding.building.elevator;
 
 import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
